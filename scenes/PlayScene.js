7b79fcad440bf8425ec2ed192e4b49b1f2d0662e
Jason McLeod <pixelparty@gmail.com>
2018-04-16 00:44:58 -0400

    messing with layout

--- scenes/PlayScene.js
+++ scenes/PlayScene.js
@@ -11,13 +11,24 @@ class PlayScene {
     }
 
     build() {
-        this.scene.canvas = blessed.box({
+        this.scene.bg = blessed.box({
             parent: this.client.screen,
+            width: '100%',
+            height: '100%',
+            top:0,
+            left:0,
+            style: {
+                bg: 'black'
+            }
+        });
+
+        this.scene.canvas = blessed.box({
+            parent: this.scene.bg,
             tags: true,
             width: 72,
             height: C.CAMERA_HEIGHT,
-            left: 3,
-            top: 2,
+            left: 2,
+            top: 1,
             keys:true,
             style: {
                 bg:'grey',
@@ -27,49 +38,89 @@ class PlayScene {
             }            
         });
 
-        this.scene.inventory = blessed.box({
-            parent: this.client.screen,
+        this.scene.character = blessed.box({
+            parent: this.scene.bg,
             tags: true,
-            width: (C.INVENTORY_WIDTH * 3) + 1,
-            height: 9,
-            right:3,
-            top:2,
-            label:'Inventory',            
+            width: 45,
+            height: 12,
+            left:76,
+            top: 1,
             border:'bg',
             style:{
                 bg:'black',
                 border:{
-                    bg:'black',
-                    ch: ' ',
+                    bg: 'grey',
+                    ch:' '
                 }
-            }
+            },
+            padding:1,
+            content: [
+                '{center}Jeezle - Level 12 Warrior{/center}',
+                '',
+                'HP {red-bg}          {/red-bg}             002500 / 005000',
+                'XP {green-bg}     {/green-bg}                    1250 /   5000',
+                '',
+                'Strength:     100      Dexterity:     100',
+                'Intelligence:  100     Constitution:  100',
+                'Armor:  100{/white-fg}'
+            ].join('\n')
         });
 
-        this.scene.equipment = blessed.box({
-            parent: this.client.screen,
+        this.scene.target = blessed.box({
+            parent: this.scene.bg,
             tags: true,
-            width: 18,
-            height: 9,
-            right:30,
-            top:2,
-            label:'Equipment',
+            width: 45,
+            height: 13,
+            left:76,
+            top: 12,
             border:'bg',
             style:{
                 bg:'black',
                 border:{
-                    bg:'black',
-                    ch: ' ',
+                    bg: 'grey',
+                    ch:' '
                 }
-            }
+            },
+            padding:1,
+            content: [
+                '{center}Target: Skeleton - Level 10 Undead{/center}',
+                '',
+                'HP {red-bg}          {/red-bg}             002500 / 005000',
+                '',
+                '{red-fg}Skeleton: 12{/red-fg}',
+                '{green-fg}Skeleton: 12{/green-fg}',
+                '{green-fg}Skeleton: 12{/green-fg}',
+                '{red-fg}Bat: 2{/red-fg}',
+                '{red-fg}Bat: 2{/red-fg}'
+            ].join('\n')
         });
+
+        // this.scene.inventory = blessed.box({
+        //     parent: this.scene.bg,
+        //     tags: true,
+        //     width: 18,
+        //     height: 11,
+        //     right:3,
+        //     top: 15,
+        //     label:'Inventory',            
+        //     border:'bg',
+        //     style:{
+        //         bg:'black',
+        //         border:{
+        //             bg:'black',
+        //             ch: ' ',
+        //         }
+        //     }
+        // });
 
         this.scene.console = blessed.box({
-            parent: this.client.screen,
+            parent: this.scene.bg,
             tags: true,
-            height: 9,
+            height: 11,
             width: 117,
-            left:3,
-            bottom:2,
+            left:2,
+            padding: 1,
+            bottom:1,
             style:{
                 bg:237,
                 border:{
@@ -77,15 +128,14 @@ class PlayScene {
                     ch: ' ',
                 }
             }
-            // content: 'Welcome to Braiv Mud!'
         });
 
         this.scene.commandPrompt = blessed.textbox({
+            parent: this.scene.bg,
             inputOnFocus: true,
-            parent: this.client.screen,
             border: 'line',
             height: 3,
-            width: 117,
+            width: 115,
             top: 29,
             left: 3,
             label: ' {white-fg}Enter command{/white-fg} ',
@@ -94,6 +144,8 @@ class PlayScene {
             keys: true,
             hidden: true
         });
+
+
 
         this.scene.canvas.key('left', () => { playerService.tryMove(this.client, -1, 0); });
         this.scene.canvas.key('up', () => { playerService.tryMove(this.client, 0, -1); });  
